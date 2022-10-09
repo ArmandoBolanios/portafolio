f71d7835f953965737ffecbf3ed1f25a4f1c8b65
ArmandoBolanios <mordana_93@hotmail.com>
2022-10-09 16:00:17 -0500

    Se modificó el apartado de resume.php

--- templates/resume.php
+++ templates/resume.php
@@ -7,15 +7,6 @@
 
             <div class="row">
                 <div class="col-lg-6" data-aos="fade-up">
-                    <h3 class="resume-title">Resumen</h3>
-                    <div class="resume-item pb-0">
-                        <h4>Armando Bolaños</h4>                        
-                        <p><em>Desarrollador Web Fullstack</em></p>
-                        <ul>                            
-                            <li>armando_isic@outlook.com</li>
-                        </ul>
-                    </div>
-
                     <h3 class="resume-title">Educación</h3>
                     <div class="resume-item">
                         <h4>Ingeniería en Sistemas Computacionales</h4>
@@ -28,6 +19,28 @@
                         <h5>2009 - 2012</h5>
                         <p><em>Colegio de Bachilleres Guerrero, México</em></p>
                         <p>Técnico en Informática</p>
+                    </div>
+                    <h3 class="resume-title">Certificaciones</h3>
+                    <div class="resume-item pb-0">
+                        <h4>JavaScript Esencial</h4>
+                        <p><em>Enero - 2022</em></p>
+                        <ul>
+                            <p>Linkedin Learning</p>
+                        </ul>
+                    </div>
+                    <div class="resume-item pb-0">
+                        <h4>Algoritmos y Estructuras de Datos JavaScript</h4>
+                        <p><em>Mayo - 2021</em></p>
+                        <ul>
+                            <p>freeCodeCamp</p>
+                        </ul>
+                    </div>
+                    <div class="resume-item pb-0">
+                        <h4>Diseño Web Responsivo</h4>
+                        <p><em>Mayo - 2021</em></p>
+                        <ul>
+                            <p>freeCodeCamp</p>
+                        </ul>
                     </div>
                 </div>
                 <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
@@ -42,6 +55,16 @@
                             <li>Metodologías de desarrollo de software</li>
                             <li>Desarrollo web front-end</li>
                             <li>Desarrollo web back-end</li>
+                        </ul>
+                    </div>
+                    <div class="resume-item">
+                        <h4>Programador Junior Full stack</h4>
+                        <h5>Septiembre 2021 - Septiembre 2022</h5>
+                        <p><em>TICS SOLUCIONES - Tlapa de Comonfort</em></p>
+                        <ul>
+                            <li>Maquetación de páginas web con HTML5, CSS3</li>
+                            <li>Desarrollo web front-end con JavaScript, JQuery y Ajax</li>
+                            <li>Desarrollo web back-end con PHP y gestor de bases de datos MySQL</li>
                         </ul>
                     </div>
                     <div class="resume-item">
